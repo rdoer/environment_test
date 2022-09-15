1425d937aac6b4e09fe3b068699bccf18fc143f3
rdoerre8 <rdoerre8@tamu.edu>
2022-09-14 21:51:00 -0500

    add attributes

--- test_app/db/schema.rb
+++ test_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_26_163121) do
+ActiveRecord::Schema.define(version: 2022_09_11_191124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 2022_08_26_163121) do
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
+    t.float "price"
+    t.date "published"
   end
 
 end
